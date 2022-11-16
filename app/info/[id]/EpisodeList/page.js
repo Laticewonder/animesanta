8dde83e2795b9319ea7af4ec087fc7a1f64e4597
Laticewonder <88091076+Laticewonder@users.noreply.github.com>
2022-11-16 23:51:44 +0530

    change ep dimension

--- app/info/[id]/EpisodeList/page.js
+++ app/info/[id]/EpisodeList/page.js
@@ -8,7 +8,7 @@ const Episodes = () => {
             <div className="cards grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 ">
                 {info.episodes.map(ep => (
                     <div className="card w-full p-1">
-                        <div className="image w-full h-36 relative rounded-lg truncate">
+                        <div className="image w-full h-28 relative rounded-lg truncate">
                             <Image src={ep.image} layout='fill' className='object-cover'/>
                         </div>
                         <h1 className='title-third' >{ep.number}.{ep.title}</h1>
