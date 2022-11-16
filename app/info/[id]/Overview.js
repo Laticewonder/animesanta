7fdb8f18db5f6dd60de3762b41cbad6ab1fe8108
Laticewonder <88091076+Laticewonder@users.noreply.github.com>
2022-11-16 17:13:52 +0530

    filled overview

--- app/info/[id]/Overview.js
+++ app/info/[id]/Overview.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image';
 import info from '../../../data/info.json'
 import parse from 'html-react-parser';
 import { useState } from 'react';
@@ -14,16 +15,54 @@ const Overview = () => {
     }
 
     return (
-        <div className="overview mb-44 min-h-fit max-h-fit max-w-fit overflow-hidden m-4 mt-8 ">
+        <div className="overview mb-44 min-h-fit max-w-6xl md:mx-auto overflow-hidden m-4 mt-8 space-y-8 ">
             <div className="description bg-zinc-800 p-4 pb-0 rounded-lg">
-                <div  className="para overflow-hidden" style={{height: showMe ? "max-content" : "100px"}} >
+                <div className="para overflow-hidden" style={{ height: showMe ? "max-content" : "100px" }} >
                     {parse(`<p className='opacity-80 text-sm tracking-wide mb-4 mx-auto max-w-prose '>${info.description}</p>`)}
                 </div>
-                <button onClick={toggle} className=' bg-zinc-800 mx-auto w-full pt-2 text-purple-500'>{showMe ? <IoIosArrowUp className=' w-full text-2xl'/>: <IoIosArrowDown className=' w-full text-2xl'/>}</button>
+                <button onClick={toggle} className=' bg-zinc-800 mx-auto w-full pt-2 text-purple-500'>{showMe ? <IoIosArrowUp className=' w-full text-2xl' /> : <IoIosArrowDown className=' w-full text-2xl' />}</button>
             </div>
 
-            <div className="character">
+            <div className="relations ">
+                <h1 className='mb-2 font-semibold text-lg'>Relations</h1>
+                <div className="cards flex gap-4">
+                    {info.relations.map(item => (
+                        <div className="card">
+                            <div className="image relative w-28 h-40 rounded-md truncate">
+                                <Image src={item.image} layout='fill' className='object-cover' />
+                            </div>
+                            <h1 className='text-xs lg:text-sm font-medium mt-2 opacity-90'>{item.title.userPreferred}</h1>
+                        </div>
+                    ))}
+                </div>
+            </div>
 
+            <div className="characters ">
+                <h1 className='mb-2 font-semibold text-lg'>Characters</h1>
+                <div className="cards flex overflow-x-auto gap-4 ">
+                    {info.characters.map((item=>(
+                        <div className="card">
+                            <div className="image w-24 h-24 relative rounded-lg truncate mb-2 ">
+                                <Image src={item.image} layout='fill' className='object-cover'/>
+                            </div>
+                            <h1 className='text-xs lg:text-sm opacity-90 tracking-wide'>{item.name.full}</h1>
+                        </div>
+                    )))}
+                </div>
+            </div>
+
+            <div className="recomendations">
+                <h1 className='mb-2 font-semibold text-lg'>Recommendations</h1>
+                <div className="cards overflow-x-auto  flex gap-4">
+                    {info.recommendations.map(item => (
+                        <div className="card">
+                            <div className="image relative w-28 h-40 rounded-md truncate">
+                                <Image src={item.image} layout='fill' className='object-cover' />
+                            </div>
+                            <h1 className='text-xs lg:text-sm font-medium mt-2 opacity-90'>{item.title.userPreferred}</h1>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
