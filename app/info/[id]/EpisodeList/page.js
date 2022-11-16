01aaa73cc9c2cc7727d89daa3aff563cb53b7179
Laticewonder <88091076+Laticewonder@users.noreply.github.com>
2022-11-17 00:01:34 +0530

    not imp

--- app/info/[id]/EpisodeList/page.js
+++ app/info/[id]/EpisodeList/page.js
@@ -4,7 +4,7 @@ import info from '../../../../data/info.json'
 const Episodes = () => {
     return (
         <div className="episodes p-4 mt-4 lg:mt-8 max-w-6xl mx-auto">
-            <h1 className='mb-4'>Epsiodes</h1>
+            <h1 className='mb-4 border-b-2 border-purple-500 p-2'>Epsiodes</h1>
             <div className="cards grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 ">
                 {info.episodes.map(ep => (
                     <div className="card w-full p-1">
